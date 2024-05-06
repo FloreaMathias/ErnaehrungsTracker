0265b11ffb384d4611687d1b845db57cc3203ced
Levi POP <levi.pop@student.htl-rankweil.at>
2024-05-06 08:42:11 +0200

    187

--- ErnaehrungsTracker/MainWindow.xaml.cs
+++ ErnaehrungsTracker/MainWindow.xaml.cs
@@ -20,6 +20,6 @@ namespace ErnaehrungsTracker
         {
             InitializeComponent();ValidateValueCallback:; 
         }
-        //kjhsdfglk jhsdflgkjhdlfkjghsldkfjghgbddddddddddddddd
+        //kjhsdfglk jhsdflgkjhdlfkjghsldkfjghgbdddddddddddddddddd
     } 
 }
