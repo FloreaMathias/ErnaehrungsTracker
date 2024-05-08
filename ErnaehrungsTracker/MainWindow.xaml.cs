cf5ed6af4b7ac5b840d584d96ad80b7c652e7cb6
Levi POP <levi.pop@student.htl-rankweil.at>
2024-05-08 14:32:25 +0200

    vieles

--- ErnaehrungsTracker/MainWindow.xaml.cs
+++ ErnaehrungsTracker/MainWindow.xaml.cs
@@ -20,6 +20,34 @@ namespace ErnaehrungsTracker
         {
             InitializeComponent();ValidateValueCallback:; 
         }
-        //kjhsdfglk jhsdflgkjhdlfkjghsldkfjghgbdddddddddddddddddd
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow firstscreen = new MainWindow();
+            firstscreen.Show();
+        }
+
+        private void mam_Click(object sender, RoutedEventArgs e)
+        {
+            Breakfast breakfast = new Breakfast();
+            breakfast.Show();
+        }
+
+        private void mama_Click(object sender, RoutedEventArgs e)
+        {
+            Lunch lunch = new Lunch();
+            lunch.Show();
+        }
+
+        private void mamam_Click(object sender, RoutedEventArgs e)
+        {
+            Dinner dinner = new Dinner();
+            dinner.Show();
+        }
+
+        private void mamama_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     } 
 }
